c2ab19d306f0729acd88e34fddd9a508fa3101bd
iwillteixeira <iwill2005@gmail.com>
2021-11-17 00:47:04 -0300

    Fix links path

--- src/js/team.js
+++ src/js/team.js
@@ -3,25 +3,25 @@ const team = [
     name: 'John Doe',
     profission: 'Develpoper',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-    image: '/dist/assets/img/pales1.png',
+    image: './dist/assets/img/pales1.png',
   },
   {
     name: 'Brad Doe',
     profission: 'Front-End',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione odio magnam.',
-    image: '/dist/assets/img/pales2.png',
+    image: './dist/assets/img/pales2.png',
   },
   {
     name: 'Anne Doe',
     profission: 'instructional designer',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione odio magnam perspiciatis, ipsam obcaecati id.',
-    image: '/dist/assets/img/pales3.png',
+    image: './dist/assets/img/pales3.png',
   },
   {
     name: 'Marry Doe',
     profission: 'Develpoper',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione odio magnam perspiciatis.',
-    image: '/dist/assets/img/pales4.png',
+    image: './dist/assets/img/pales4.png',
   },
 
 ];
